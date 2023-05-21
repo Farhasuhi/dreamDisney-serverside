e9fe7fbc136767b7c72832ff818fcf603d96d16c
Farhasuhi <git config --global user.email suhifarhaislam@gmail.com>
2023-05-21 20:10:42 +0600

    fixing some issue

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import useTittle from '../../hooks/useTittle';
 
 const Blog = () => {
-    useTittle('blog')
+    useTittle('blog');
     return (
         <div className='md:w-[90%] w-[95%] mx-auto'>
             <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
